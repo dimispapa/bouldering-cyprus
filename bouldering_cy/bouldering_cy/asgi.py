23535fb989436462cb95a84850c9a97d0a2a456d
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2025-01-13 15:49:25 +0000

    chore: set up django project

---
+++ bouldering_cy/bouldering_cy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bouldering_cy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bouldering_cy.settings')
+
+application = get_asgi_application()
