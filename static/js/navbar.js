94b57033ea4348640ea0c2b881df35ba8cb46d83
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2025-01-17 18:18:35 +0000

    style: update navbar styles

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('.navbar')
 
-  const toggleNavbarBackground = () => {
+  const toggleNavbarState = () => {
     if (window.scrollY > 50) {
-      navbar.classList.add('navbar-scrolled') // Add solid background
-      navbar.classList.remove('navbar-transparent') // Remove transparency
+      navbar.classList.add('navbar-scrolled') // Add scrolled state
+      navbar.classList.remove('navbar-transparent') // Remove transparent state
     } else {
-      navbar.classList.add('navbar-transparent') // Restore transparency
-      navbar.classList.remove('navbar-scrolled') // Remove solid background
+      navbar.classList.add('navbar-transparent') // Add transparent state
+      navbar.classList.remove('navbar-scrolled') // Remove scrolled state
     }
   }
 
   // Run on page load and on scroll
-  toggleNavbarBackground()
-  window.addEventListener('scroll', toggleNavbarBackground)
+  toggleNavbarState()
+  window.addEventListener('scroll', toggleNavbarState)
 })
