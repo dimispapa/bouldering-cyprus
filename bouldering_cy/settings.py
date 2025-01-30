aa9df881b6885002e15ba735fe3f04a131dd8c0f
dimispapa <dpapakyriacou14@gmail.com>
2025-01-30 19:23:56 +0200

    chore: update settings to test for s3

--- bouldering_cy/settings.py
+++ bouldering_cy/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -165,6 +164,7 @@ if PRODUCTION:
 # Local Development
 else:
     STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
