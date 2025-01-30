4ed183c37dd1a412f1511a8b949b6b14bbd22864
dimispapa <dpapakyriacou14@gmail.com>
2025-01-30 11:02:23 +0200

    chore: set default secret key

--- bouldering_cy/settings.py
+++ bouldering_cy/settings.py
@@ -27,7 +27,7 @@ ENVIRONMENT = os.environ.get("DJANGO_ENV", "DEV")  # Default to 'DEV'
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENVIRONMENT == "DEV"
