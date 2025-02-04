0227d0dbb5932c46c4c96d0c5e1a372bec2936b4
dimispapa <dpapakyriacou14@gmail.com>
2025-02-04 18:15:12 +0200

    feat: add hero imgs to cart page

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cart/", views.cart_detail, name="cart_detail"),
-    path("cart/add/<int:product_id>/", views.cart_add, name="cart_add"),
-    path("cart/remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
+    path("", views.cart_detail, name="cart_detail"),
+    path("add/<int:product_id>/", views.cart_add, name="cart_add"),
+    path("remove/<int:product_id>/", views.cart_remove, name="cart_remove"),
 ]
