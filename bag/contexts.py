f2180ea2e2ad17c39754584e63b13e922b336f4f
dimispapa <dpapakyriacou14@gmail.com>
2025-02-04 14:10:00 +0200

    chore: remove code related to cart
    compatibility issues of cart lib with django version

--- bag/contexts.py
+++
@@ -1,6 +0,0 @@
-from carton.cart import Cart
-
-
-def cart_item_count(request):
-    cart = Cart(request.session)
-    return {'cart_item_count': len(cart)}
--- bag/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("bag/", views.bag, name="bag"),
-    path("bag/add/<int:product_id>/", views.add_to_bag, name="add_to_bag"),
-]
