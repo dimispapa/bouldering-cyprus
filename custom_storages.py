67fb9e0e15acb41e41e00248e3c902e6ce7778ed
dimispapa <dpapakyriacou14@gmail.com>
2025-02-04 14:45:56 +0200

    chore: revert back to S3 settings to check

--- custom_storages.py
+++ custom_storages.py
@@ -6,11 +6,11 @@ class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     default_acl = "public-read"
     file_overwrite = True  # Ensures old files are replaced
-    custom_domain = settings.AWS_CLOUDFRONT_DOMAIN  # Use CloudFront
+    # custom_domain = settings.AWS_CLOUDFRONT_DOMAIN  # Use CloudFront
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
     default_acl = "public-read"
     file_overwrite = False  # Prevents overwriting existing media files
-    custom_domain = settings.AWS_CLOUDFRONT_DOMAIN  # Use CloudFront
+    # custom_domain = settings.AWS_CLOUDFRONT_DOMAIN  # Use CloudFront
