737ad0b56e0a0ef5ba651343e76aab37a565448a
dimispapa <dpapakyriacou14@gmail.com>
2025-02-07 16:50:52 +0200

    feat: create order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address_line1 = models.CharField(max_length=250)
+    address_line2 = models.CharField(max_length=250, blank=True)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    stripe_piid = models.CharField(max_length=255, null=False, blank=False, default="")
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f"Order {self.order_number}"
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
