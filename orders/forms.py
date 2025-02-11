b85df460084e0555a3cdb6faf033fe03e2d102ae
dimispapa <dpapakyriacou14@gmail.com>
2025-02-11 16:41:57 +0200

    feat: remove form labels + add placeholders
    also refine the form layout

--- orders/forms.py
+++ orders/forms.py
@@ -23,21 +23,45 @@ class OrderForm(forms.ModelForm):
             "phone",
             "address_line1",
             "address_line2",
-            "city",
+            "town_or_city",
             "postal_code",
             "country",
         ]
+
+    def set_placeholders(self, placeholders):
+        """Set placeholders for form fields."""
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f"{placeholders[field]} *"
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].label = False
 
     def __init__(self, *args, **kwargs):
         stripe_public_key = kwargs.pop("stripe_public_key", "")
         stripe_client_secret = kwargs.pop("stripe_client_secret", "")
 
         super().__init__(*args, **kwargs)
+        placeholders = {
+            "first_name": "First name",
+            "last_name": "Last name",
+            "email": "Email address",
+            "phone": "Phone number",
+            "address_line1": "Street address 1",
+            "address_line2": "Street address 2",
+            "town_or_city": "Town or city",
+            "postal_code": "Postal code",
+            "country": "Country",
+        }
+        # Set autofocus on first name field
+        self.fields["first_name"].widget.attrs["autofocus"] = True
+        # Set placeholders for form fields
+        self.set_placeholders(placeholders)
+        # Set helper for form
         self.helper = FormHelper()
-        self.helper.form_method = "post"
         self.helper.form_id = "payment-form"
-        self.helper.form_action = "#"  # or your specific action URL
-
+        # Set layout for form
         self.helper.layout = Layout(
             # Customer Details Section
             Fieldset(
@@ -52,15 +76,20 @@ class OrderForm(forms.ModelForm):
                     Column("phone", css_class="form-group col-md-6"),
                     css_class="form-row",
                 ),
+            ),
+            HTML("""<hr>"""),
+            Fieldset(
+                "Delivery address",
                 Field("address_line1"),
                 Field("address_line2"),
                 Row(
-                    Column("city", css_class="form-group col-md-6"),
+                    Column("town_or_city", css_class="form-group col-md-6"),
                     Column("postal_code", css_class="form-group col-md-3"),
                     Column("country", css_class="form-group col-md-3"),
                     css_class="form-row",
                 ),
             ),
+            HTML("""<hr>"""),
             # Payment Details Section
             Fieldset(
                 "Payment details",
@@ -72,7 +101,6 @@ class OrderForm(forms.ModelForm):
                         <div id="express-checkout-element" class="stripe-element border border-dark rounded p-3">
                             <!-- Express Checkout Element will be inserted here -->
                         </div>
-                        <div id="express-checkout-errors" role="alert" class="mt-2 text-danger"></div>
                     </div>
                 """
                 ),
@@ -80,14 +108,14 @@ class OrderForm(forms.ModelForm):
                 HTML(
                     """
                     <div class="form-group mb-4">
-                        <label for="payment-element">Other payment methods</label>
+                        <label for="payment-element">OR other payment methods</label>
                         <div id="payment-element" class="stripe-element border border-dark rounded p-3">
                             <!-- Payment Element will be inserted here -->
                         </div>
-                        <div id="payment-errors" role="alert" class="mt-2 text-danger"></div>
                     </div>
                 """
                 ),
+                HTML("""<div id="payment-errors" role="alert" class="mt-2 text-danger hidden"></div>"""),
                 # Hidden Stripe Fields
                 Hidden("stripe-public-key", stripe_public_key, id="stripe-public-key"),
                 Hidden(
@@ -96,11 +124,15 @@ class OrderForm(forms.ModelForm):
                     id="stripe-client-secret",
                 ),
             ),
+            # Place Order Button
             Div(
                 HTML(
                     """
-                    <button type="submit" name="submit" class="button-payment mt-3">
-                        <i class="fa-solid fa-lock"></i> Place order
+                    <button id="submit" type="submit" name="submit" class="button-payment mt-3">
+                        <span id="spinner" class="spinner-border spinner-border-sm hidden" role="status" aria-hidden="true"></span>
+                        <span id="button-text">
+                            <i class="fa-solid fa-lock"></i> Place order
+                        </span>
                     </button>
                 """
                 ),
