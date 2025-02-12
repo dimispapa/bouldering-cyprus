8ea0faa28633a756aa9e734b0bcc524d0e383b0e
dimispapa <dpapakyriacou14@gmail.com>
2025-02-12 11:40:49 +0200

    feat: set up admin page for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+    def formfield_callback(self, db_field, formfield, request):
+        if formfield:
+            formfield.widget.attrs['onchange'] = 'this.form.submit()'
+        return formfield
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "order_number",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "date_created",
+        "date_updated",
+        "grand_total",
+    ]
+    list_filter = ["date_created"]
+    search_fields = [
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+        "order_number",
+    ]
+    inlines = [OrderItemInline]
+
+    def save_formset(self, request, form, formset, change):
+        formset.save()
+        for instance in formset.forms:
+            if instance.instance.order:
+                instance.instance.order.update_total()
