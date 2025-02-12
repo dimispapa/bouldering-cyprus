77dfa854d1778124909c4856363f0fc4a67839e6
dimispapa <dpapakyriacou14@gmail.com>
2025-02-12 11:40:16 +0200

    feat: increase delay for toasts autohide

--- static/js/toasts.js
+++ static/js/toasts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toastElList = document.querySelectorAll('.toast')
-  const option = {animation: true, autohide: true, delay: 5000}
+  const option = {animation: true, autohide: true, delay: 10000}
   const toastList = [...toastElList].map(toastEl =>
     new bootstrap.Toast(toastEl, option).show()
   )
