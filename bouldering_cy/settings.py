276ff94206d771471f4c9401f8c9036cd5e67a5d
dimispapa <dpapakyriacou14@gmail.com>
2025-02-12 14:41:09 +0200

    chore: update email port settings

--- bouldering_cy/settings.py
+++ bouldering_cy/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_KEY")
@@ -122,6 +122,7 @@ SERVER_EMAIL = os.environ.get("DEFAULT_EMAIL")
 
 if not PRODUCTION:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_PORT = 587
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
