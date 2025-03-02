1cbefcb31555f508428e85ea8f3c51ee04b69b9c
dimispapa <dpapakyriacou14@gmail.com>
2025-03-02 18:18:24 +0000

    feat: integrate newsletter with user signup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff',
+                    'is_active')
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email', )
+
+
+# Unregister the default UserAdmin and register our custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from .views import CustomSignupView
 app_name = 'accounts'
 
 urlpatterns = [
     path('delete-account/', views.delete_account, name='delete_account'),
+    path('signup/', CustomSignupView.as_view(), name='account_signup'),
 ]
